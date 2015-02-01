38adc2e3dd1a177158cc96289b8fd3fdb987daa0
xiaomaogy <xiaomaogy88@gmail.com>
2015-01-31 19:49:39 -0600

    minDepthBinary Tree

---
+++ LeetCode/src/minDepthBinaryTree/Solution.java
@@ -0,0 +1,17 @@
+package minDepthBinaryTree;
+
+public class Solution {
+	public int minDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        
+        if(root.left!=null && root.right!=null ){
+            return 1+Math.min(minDepth(root.left), minDepth(root.right));
+        }else if(root.left==null){
+            return 1+minDepth(root.right);
+        }else{
+            return 1+minDepth(root.left);
+        }
+    }
+}
